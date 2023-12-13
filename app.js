57f24252dc4eca1202aa4ad86886619691e1f683
Ibrahim Younes Zaidi <145864227+Ibrahim-Zaidi@users.noreply.github.com>
2023-12-13 13:38:27 +0100

    Day_? :)

---
+++ app.js
@@ -0,0 +1,23 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const openModal = document.querySelector('.modal-btn');
+const closeModal = document.querySelector('.close-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+openModal.addEventListener('click', function() {
+    modalOverlay.classList.toggle('open-modal');
+})
+
+closeModal.addEventListener('click', function () {
+    modalOverlay.classList.remove('open-modal');
+})
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && modalOverlay.classList.contains('open-modal')) {
+        modalOverlay.classList.remove('open-modal');
+        console.log(e.key);
+    }
+})
